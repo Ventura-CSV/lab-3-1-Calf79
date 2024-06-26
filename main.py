0b1703588574cd6fc11e98f3c8e710f1401c04f4
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-26 13:20:56 -0700

    Chap 3. Lab1 Camerino A.

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ def main():
     # Comlete your code here
     ##################################################
     number = int(input('Enter your input: '))
-
+    if number>=100:
+        range=3
+    else: 
+        if number>=50:
+         range=2
+        else:
+         range=1
+        
     """
     Make your code here
     """
